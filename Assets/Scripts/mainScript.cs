d96b3c0462624e90339615151b72b632d46f3b77
AtulYadav01 <ashuy033@gmail.com>
2022-01-13 11:18:15 +0530

    First Commit

---
+++ Assets/Scripts/mainScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainScript : MonoBehaviour
+{
+
+  
+    public float Speed = 10;
+    public bool isSpinning = true;
+
+    public GameObject pointer;
+
+    void Start()
+    {
+        pointer.SetActive(false);
+    }
+
+    void Update()
+    {
+        Rotate();
+    }
+
+    void Rotate()
+    {
+        transform.Rotate (0 , 2 * Time.deltaTime, Speed * Time.deltaTime, Space.Self);
+
+        if (isSpinning == false && Speed > 0)
+        {
+            Stop();
+        }
+    }
+
+   public void Stop()
+    {
+        Speed--;
+
+        if(Speed <= 0)
+        {
+            pointer.GetComponent<BoxCollider> ().enabled = true;
+            Speed = 0;
+        }
+    }
+
+}
---
+++ Assets/Scripts/pointer.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pointer : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log(col.gameObject.name);
+      
+    }
+}
